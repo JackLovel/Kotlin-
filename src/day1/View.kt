4e3e9922f7c3eef90fc1823fcca201a800fbed01
JackLovel <gogkatsu@outlook.com>
2020-02-22 14:20:36 +0800

    提交

--- src/day1/View.kt
+++ src/day1/View.kt
@@ -1,5 +1,13 @@
 package day1;
 
+import java.lang.StringBuilder
+
+var StringBuilder.lastChar : Char
+    get() = get(length - 1)
+    set(value : Char) {
+        this.setCharAt(length - 1, value)
+    }
+
 open class View {
     open fun click() = println("view click")
 }
@@ -12,8 +20,41 @@ class Button : View() {
 
 fun View.showOff() = println("我是View")
 fun Button.showOff() = println("我是Button")
+
+class User(val id : Int, val name : String, val address : String)
+
+fun saveUser(user: User) {
+//    if (user.name.isEmpty()) {
+//        throw IllegalArgumentException("Can't save user ${user.id}: empty Name")
+//    }
+//
+//    if (user.address.isEmpty()) {
+//        throw IllegalArgumentException("Can't save user ${user.address}: empty Address")
+//    }
+}
+
+fun validate(user: User,
+             value: String,
+             fieldName : String) {
+    if (value.isEmpty()) {
+        if (user.address.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${user.address}: empty Address")
+        }
+    }
+}
 fun main() {
-    val view: View = Button()
-    view.click()
-    view.showOff()
+//    val view: View = Button()
+//    view.click()
+//    view.showOff()
+
+//    val (number, name) = 1 to "one"
+//
+//    val sb = StringBuilder("Kotlin?")
+//    sb.lastChar = '!'
+//    println(sb)
+//    println("12.345-6.A".split("\\.|-".toRegex()))
+//    saveUser(User(1, "",""))
+
+    var user: User = User(1,"wcc", "sss")
+    saveUser(user)
 }
